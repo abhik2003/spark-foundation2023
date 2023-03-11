73e8f971372911c21526fd252d5f438635913dc9
Abhik Maji <abhik03mj@gmail.com>
2023-03-11 14:47:55 +0530

    heading style updated

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 elem = document.getElementById("heading-spark")
 var x = 0;
-var spark="The Spark Foundation Bank"
+var spark = "The Spark Foundation Bank"
+var flag = 1;
 dynamic_name = () => {
-    elem.innerText = "T"+spark.slice(1, x % spark.length+1)+"|";
-    x += 1;
+    elem.innerText = spark.slice(0, x % spark.length+1);
+    if (flag == 1)
+        x += 1;
+    else
+        x -= 1;    
+    if (x == spark.length-1)
+        flag = 0;
+    if (x == 0)
+        flag = 1;    
 }
 
-setInterval(dynamic_name, 200);
+setInterval(dynamic_name, 150);
